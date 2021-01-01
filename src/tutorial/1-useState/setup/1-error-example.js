34309aa76bf33155a8c50ff03a79eb5106d286e0
Apple <sdjee2015@gmail.com>
2021-01-01 15:55:29 +0530

    Problem with not using states and just changing variable value

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+
+var title = "Hello world!";
+
+//Even though the variable value is changed but it did not reflect in UI
+//That is because the component is not re rendered after value was changed, so we need state change
+//State change in variable also re rerenders smartly(Virtual DOM)
+const changeTitle = () => {
+  title = "Hello Kitty!";
+  console.log(`Title changed: ${title} but did it reflect?`);
+};
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button className="btn" onClick={changeTitle}>
+        Click to change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
