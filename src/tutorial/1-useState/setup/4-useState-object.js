14d38893c30680b1436dba7276fd0841288b6c4c
Apple <sdjee2015@gmail.com>
2021-01-02 18:30:47 +0530

    useState for objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Saransh",
+    age: 24,
+    message: "Hello world!",
+  });
+
+  //Using spread operator to copy values into new object and overrwriting message property
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Hello Kitty!" });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
