6939d752eb26cc4ee5ca15a614c3883b0f409459
Apple <sdjee2015@gmail.com>
2021-01-02 19:21:48 +0530

    State Functional update

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 
+//Functional Update - we use a fn inside setCount to return the new value
+//The problem with normal setCount will be that it is async, if we click multiple times and add a timer, it will only increment by 1
+//Functional update is sync, i.e it will always take the updated value
 const UseStateCounter = () => {
   const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setTimeout(() => {
+      //setCount(count + 1);
+
+      //Functional update
+      setCount((prevCount) => {
+        return prevCount + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <>
       <h2>Regular counter</h2>
@@ -12,7 +27,7 @@ const UseStateCounter = () => {
       <button className="btn" onClick={() => setCount(0)}>
         Reset
       </button>
-      <button className="btn" onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={increment}>
         Increase
       </button>
     </>
