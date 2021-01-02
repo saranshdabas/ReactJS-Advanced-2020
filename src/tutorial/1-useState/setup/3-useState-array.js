393f8e1080f91f220ff909304413d04b97f3b1a5
Apple <sdjee2015@gmail.com>
2021-01-02 17:32:24 +0530

    Use state on arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
+//Using filter to set only people who do not have the id which we are going to delete
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+    const newPeople = people.filter((people) => people.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <>
+      {people.map((people) => {
+        const { id, name } = people;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        );
+      })}
+      <button
+        className="btn"
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        Remove all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
