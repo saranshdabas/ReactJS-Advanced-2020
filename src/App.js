e48dafafe68f4b2df46f6e59201cf01207d83297
Apple <sdjee2015@gmail.com>
2021-01-07 21:20:53 +0530

    useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UseEffect from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+import UseEffect from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 function App() {
   return (
     <div className="container">
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
-// second argument
-
+// First time event listener is setup on page reload
+// When width is change setWidth is called, when setWidth is called again useEffect is called
+// This leads to tons of even listeners being setup hence memory leak
+// To solve this we can remove the eventListener each time we add it.
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const changeSize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", changeSize);
+    return () => {
+      window.removeEventListener("resize", changeSize);
+    };
+  });
+
+  return (
+    <>
+      <h3>Width</h3>
+      <h4>{width}</h4>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
