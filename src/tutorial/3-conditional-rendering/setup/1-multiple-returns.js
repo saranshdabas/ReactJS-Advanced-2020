e421294f51919563f47d354b89efa8cf0b3690d6
Apple <sdjee2015@gmail.com>
2021-01-09 23:16:05 +0530

    Conditional Rendering Multiple events

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,13 +1,55 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
+
+//Conditional rendering with error and loading
+
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        //Error handling type 1, 404 etc (fetch api format)
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        }
+
+        setLoading(false);
+        setError(true);
+        throw new Error(resp.statusText);
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      //Error handling type 2, network issues(fetch api format)
+      .catch((err) => console.log(err));
+  }, []);
 
   if (loading) {
-    return <h2>Loading...</h2>;
-  } else {
-    return <h2>Hello World!</h2>;
+    return (
+      <>
+        <h3>Loading...</h3>
+      </>
+    );
   }
+
+  if (error) {
+    return (
+      <>
+        <h3>Error...</h3>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3>{user}</h3>
+    </>
+  );
 };
 
 export default MultipleReturns;
