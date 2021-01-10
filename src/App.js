024343956345395bacaf97b5665cc758de7527b0
Apple <sdjee2015@gmail.com>
2021-01-10 10:41:38 +0530

    Short-circuit evaluation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 function App() {
   return (
     <div className="container">
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  const [text, setText] = useState("ABC");
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h2>short circuit</h2>
+      <h3>{text || "Saransh"}</h3>
+      {text && <h3>{text}</h3>}
+      {!text && <h3>Hello world</h3>}
+    </>
+  );
 };
 
 export default ShortCircuit;
