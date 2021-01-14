cfe71b8d53f0f5accef5f03cc51802946544a000
Apple <sdjee2015@gmail.com>
2021-01-14 21:10:47 +0530

    Basic forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
-      <ConditionalRendering></ConditionalRendering>
+      <Setup></Setup>
     </div>
   );
 }
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
 
+//By default after handle submit the page rereshes
+//To prevent and see console log we prevent Default behavior
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello world!");
+  };
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <button type="submit">Add person</button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
