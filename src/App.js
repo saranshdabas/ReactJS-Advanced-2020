a1044feae2dfa48c54cade6dc8366cc2b5edd15c
Apple <sdjee2015@gmail.com>
2021-01-19 20:34:23 +0530

    Initial setup for useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Setup from "./tutorial/6-useReducer/setup";
+
+//If the file name is not speficied, index.js will be used as entry point
 function App() {
   return (
     <div className="container">
