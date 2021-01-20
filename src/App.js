cfc5b99253a81d40cc7ee3d4616c506d540c08ad
Apple <sdjee2015@gmail.com>
2021-01-21 00:32:59 +0530

    Prop drilling problem in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 
 //If the file name is not speficied, index.js will be used as entry point
 function App() {
--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
-// more components
-// fix - context api, redux (for more complex cases)
-
+//Prop drilling is a problem
+//If there is a chain of components and we want a call a fn from last component, we need to pass down that fn
+//This problem is solved by Context API.
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <section>
+      <h3>Prop drilling</h3>
+      <List people={people} removeItem={removeItem} />
+    </section>
+  );
+};
+
+//Even though List component don't need removeItem fn, it still needs it to pass it to SinglePerson component.
+const List = ({ people, removeItem }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson key={person.id} {...person} removeItem={removeItem} />
+        );
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name, removeItem }) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button className="btn" onClick={() => removeItem(id)}>
+        Remove
+      </button>
+    </div>
+  );
 };
 
 export default PropDrilling;
