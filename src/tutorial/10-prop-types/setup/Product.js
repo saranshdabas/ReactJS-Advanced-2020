0b747e51762be18ad45087dfca2680de52c0ac5e
Apple <sdjee2015@gmail.com>
2021-01-23 00:13:31 +0530

    Default props in ReactJS

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
 //Sometimes APIs will not have all the data
 //In our case image and price is missing from one of the values
@@ -9,6 +10,7 @@ const Product = ({ name, image, price }) => {
   return (
     <article className="product">
       <h4>{name}</h4>
+      <img src={image.url} alt={name}></img>
       <p>{price}</p>
     </article>
   );
@@ -17,7 +19,14 @@ const Product = ({ name, image, price }) => {
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.object.isRequired,
-  price: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+//Deafult props saves us in case of missing props
+Product.defaultProps = {
+  name: "Default name",
+  image: { url: defaultImage },
+  price: 3.99,
 };
 
 export default Product;
