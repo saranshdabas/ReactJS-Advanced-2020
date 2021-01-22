2e9a80c4ce6490323773596be095334f2f539340
Apple <sdjee2015@gmail.com>
2021-01-22 22:21:35 +0530

    Custom hook in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 
 //If the file name is not speficied, index.js will be used as entry point
 function App() {
--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+//Custom hooks needs to start with use
+//Because other hooks can only be called inside a component or fn starting with use
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+
+  return { loading, products };
+};
