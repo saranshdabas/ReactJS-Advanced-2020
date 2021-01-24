9a0b4173cbd8bb88ec64100e78ecd39e2ddc55c5
Apple <sdjee2015@gmail.com>
2021-01-24 11:12:46 +0530

    React router switch component

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,17 +10,27 @@ import Person from "./Person";
 // navbar
 import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
+  //We need to give exact for / because in /about and /people, / will also get matched
+  //Hence Home component will be displayed with every other component.
+  //To display error component we can match with *(everything) and use switch component
+  //Switch component will help to match only the first route
+  //If nothing matches, the error component will be matched eventually
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/people">
-        <People />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 };
