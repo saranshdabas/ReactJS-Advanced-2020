e7b1231fbc09818ef6fa6495c86747e79ae6f2f4
Apple <sdjee2015@gmail.com>
2021-01-27 20:28:59 +0530

    React.memo() in ReactJS

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,48 +1,56 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
-
+//Everytime we increment count, Index component re-renders
+//This cause BigList to rerender even though nothing has changed is BigList component which then causes many Single product components to re-rerender
+//To prevent this we use React.memo fn which only re re-renders a component if props value was changed
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
       <BigList products={products} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products }) => {
+  useEffect(() => {
+    console.log("BigList was rendered");
+  }, []);
   return (
-    <section className='products'>
+    <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product}></SingleProduct>;
       })}
     </section>
-  )
-}
+  );
+});
 
 const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+  useEffect(() => {
+    console.log("SingleProduct was rendered");
+  }, []);
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
-    <article className='product'>
+    <article className="product">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
